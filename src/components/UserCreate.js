474c9aee2467eeae169db507cc23e9e64b162ec2
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-10-21 00:16:20 +0400

    cleaned up our components

--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -3,7 +3,12 @@ import Field from './Field'
 import Button from './Button'
 
 const UserCreate = () => {
-
+    return (
+        <div className="ui form">
+            <Field />
+            <Button />
+        </div>
+    )
 }
 
 export default UserCreate
